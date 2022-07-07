23e53b1bf8c4c4c4547e4e46761e155a08ab2772
kseniyaAvdeenko <ksunyaef@gmail.com>
2022-07-07 22:58:19 +0300

    request

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '7fa86abd3da2d37e62f7603cb86cec2a'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid="+appid
+    city = 'London'
     return render(request, 'weather/index.html')
