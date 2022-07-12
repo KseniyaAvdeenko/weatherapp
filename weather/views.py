91541b3af52525bb99ccd84f5c50efc596d811b2
kseniyaAvdeenko <ksunyaef@gmail.com>
2022-07-12 17:03:21 +0300

    delete cities

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import City
 from .forms import CityForm
 
 def index(request):
     appid = '7fa86abd3da2d37e62f7603cb86cec2a'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+appid
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid="+appid
     
+
+    if (request.method=="POST"):
+        form = CityForm(request.POST)
+        form.save()
+        
+    form = CityForm()
+
     cities = City.objects.all()
 
     all_cities = []
@@ -19,19 +26,19 @@ def index(request):
             'icon': res['weather'][0]['icon'],
             'wind': res['wind']['speed'],
             "pressure": res["main"]['pressure'],
-            'humidity': res['main']['humidity']
+            'humidity': res['main']['humidity'], 
         }
 
         all_cities.append(city_info)
 
-    if (request.method=="post"):
-        form = CityForm(request.POST)
-        form.save()
-    
-    form = CityForm()
-
     context = {
         'all_info': all_cities, 'form':form
     }
-    
+
     return render(request, 'weather/index.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+
+    return redirect('index')
+
